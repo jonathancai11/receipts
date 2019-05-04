fb71e9a6f44c9577cb7b47173f7a680410352dea
Jonathan Cai <caimjonathan@gmail.com>
2019-05-04 16:55:53 -0700

    include original photo

--- process.py
+++ process.py
@@ -2,5 +2,11 @@ import json
 
 with open('out.json') as json_file:  
     data = json.load(json_file)
-    print(data)
+    # print(data)
+
+    # The first "textAnnotation" contains ALL of the annotations.
+    print(data["responses"][0]["textAnnotations"][0]["description"])
     
+    # Afterwards, the annotations have the positions/descriptions of each separate annotation.
+    print("\n\nNEXT\n\n")
+    print(data["responses"][0]["textAnnotations"][1]["description"])
