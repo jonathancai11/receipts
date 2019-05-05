37678e11f21202d6a75be45323d6ba0445fb6230
Jonathan Cai <caimjonathan@gmail.com>
2019-05-05 01:15:11 -0700

    trouble authenticating and creating google bucket thru python client

--- process.py
+++ process.py
@@ -1,25 +1,45 @@
 import json
-from google.cloud import vision
+from google.cloud import vision, storage
 from google.protobuf.json_format import MessageToJson
+
+import google.auth as auth
 
 # Client to Vision API
 client = vision.ImageAnnotatorClient()
 
-# Name of file in google bucket (must be anonymously accessible)
-filename = 'gs://johnnys-receipt-bucket/receipt-ocr-original.jpg'
+# Client to Storage API
+storage_client = storage.Client()
 
-# Make request and store response
-response = client.annotate_image({
-  'image': {'source': {'image_uri': filename}},
-  'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
-})
+bucket_name = "random-bucket-name-lmao"
 
-# Serialize response
-serialized = MessageToJson(response)
+# Creates the new bucket
+bucket = storage_client.create_bucket(bucket_name)
 
-# Write serialized to json file
-with open(filename.split('/')[len(filename.split('/')) - 1] + '-data.json', 'w') as outfile:  
-    json.dump(serialized, outfile)
+
+# credentials = auth.credentials.Credentials()
+# storage_client = storage.client.Client(token="AIzaSyC6gIZ9kp98JrKTsICuEia4G_P5DAMMVmk")
+
+# projectid = "carbide-booth-239600"
+
+# bucket = storage.bucket.Bucket(storage_client, name="random-bucket-name-lmao")
+
+# bucket.create(project=projectid)
+
+# # Name of file in google bucket (must be anonymously accessible)
+# filename = 'gs://johnnys-receipt-bucket/receipt-ocr-original.jpg'
+
+# # Make request and store response
+# response = client.annotate_image({
+#   'image': {'source': {'image_uri': filename}},
+#   'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
+# })
+
+# # Serialize response
+# serialized = MessageToJson(response)
+
+# # Write serialized to json file
+# with open(filename.split('/')[len(filename.split('/')) - 1] + '-data.json', 'w') as outfile:  
+#     json.dump(serialized, outfile)
 
 
 # with open('out.json') as json_file:  
