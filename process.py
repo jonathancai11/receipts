b7fe04754ff39979abf333f5d9451de0664de6d6
Jonathan Cai <caimjonathan@gmail.com>
2019-05-05 00:14:00 -0700

    .gitignore, requirements

--- process.py
+++ process.py
@@ -1,12 +1,21 @@
 import json
+from google.cloud import vision
 
-with open('out.json') as json_file:  
-    data = json.load(json_file)
-    # print(data)
+client = vision.ImageAnnotatorClient()
+response = client.annotate_image({
+  'image': {'source': {'image_uri': 'gs://johnnys-receipt-bucket/receipt-ocr-original.jpg'}},
+  'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
+})
 
-    # The first "textAnnotation" contains ALL of the annotations.
-    print(data["responses"][0]["textAnnotations"][0]["description"])
+print(response)
+
+# with open('out.json') as json_file:  
+#     data = json.load(json_file)
+#     # print(data)
+
+#     # The first "textAnnotation" contains ALL of the annotations.
+#     print(data["responses"][0]["textAnnotations"][0]["description"])
     
-    # Afterwards, the annotations have the positions/descriptions of each separate annotation.
-    print("\n\nNEXT\n\n")
-    print(data["responses"][0]["textAnnotations"][1]["description"])
+#     # Afterwards, the annotations have the positions/descriptions of each separate annotation.
+#     print("\n\nNEXT\n\n")
+#     print(data["responses"][0]["textAnnotations"][1]["description"])
