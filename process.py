4ee6dddab3a40cf2d191ac55823d316cda1db47f
Jonathan Cai <caimjonathan@gmail.com>
2019-05-05 01:59:58 -0700

    functions to interface w gcp apis

--- process.py
+++ process.py
@@ -2,53 +2,84 @@ import json
 from google.cloud import vision, storage
 from google.protobuf.json_format import MessageToJson
 
-import google.auth as auth
 
-# Client to Vision API
-client = vision.ImageAnnotatorClient()
-
-# Client to Storage API
-storage_client = storage.Client()
-
-bucket_name = "random-bucket-name-lmao"
-
-# Creates the new bucket
-bucket = storage_client.create_bucket(bucket_name)
+def create_bucket(bucket_name):
+    """ Creates bucket with name bucket_name """
+    # Client to Storage API
+    storage_client = storage.Client()
+    # Creates the new bucket
+    bucket = storage_client.create_bucket(bucket_name)
 
 
-# credentials = auth.credentials.Credentials()
-# storage_client = storage.client.Client(token="AIzaSyC6gIZ9kp98JrKTsICuEia4G_P5DAMMVmk")
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
 
-# projectid = "carbide-booth-239600"
+    blob.upload_from_filename(source_file_name)
 
-# bucket = storage.bucket.Bucket(storage_client, name="random-bucket-name-lmao")
-
-# bucket.create(project=projectid)
-
-# # Name of file in google bucket (must be anonymously accessible)
-# filename = 'gs://johnnys-receipt-bucket/receipt-ocr-original.jpg'
-
-# # Make request and store response
-# response = client.annotate_image({
-#   'image': {'source': {'image_uri': filename}},
-#   'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
-# })
-
-# # Serialize response
-# serialized = MessageToJson(response)
-
-# # Write serialized to json file
-# with open(filename.split('/')[len(filename.split('/')) - 1] + '-data.json', 'w') as outfile:  
-#     json.dump(serialized, outfile)
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination_blob_name))
 
 
-# with open('out.json') as json_file:  
-#     data = json.load(json_file)
-#     # print(data)
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
 
-#     # The first "textAnnotation" contains ALL of the annotations.
-#     print(data["responses"][0]["textAnnotations"][0]["description"])
-    
-#     # Afterwards, the annotations have the positions/descriptions of each separate annotation.
-#     print("\n\nNEXT\n\n")
-#     print(data["responses"][0]["textAnnotations"][1]["description"])
+    blobs = bucket.list_blobs()
+
+    for blob in blobs:
+        print(blob.name)
+
+
+def annotate_image(bucket_name, file_name, output):
+    """ Annotate file_name, outputs to json if output is True """
+    # Client to Vision API
+    client = vision.ImageAnnotatorClient()
+
+    project_id = "carbide-booth-239600"
+
+    # Name of file in google bucket (must be anonymously accessible)
+    file_name = 'gs://' + bucket_name + '/' + file_name
+
+    # Make request and store response
+    response = client.annotate_image({
+        'image': {'source': {'image_uri': file_name}},
+        'features': [{'type': vision.enums.Feature.Type.TEXT_DETECTION}],
+    })
+
+    # Serialize response
+    serialized = MessageToJson(response)
+
+    if (output):
+        # Write serialized to json file
+        with open(file_name.split('/')[len(file_name.split('/')) - 1] + '-data.json', 'w') as outfile:  
+            json.dump(serialized, outfile)
+
+    return serialized
+
+
+def read_response_json(output_file_name):
+    """ Reads the response json file output_file_name """
+    with open('out.json') as json_file:  
+        data = json.load(json_file)
+        # print(data)
+
+        # The first "textAnnotation" contains ALL of the annotations.
+        print(data["responses"][0]["textAnnotations"][0]["description"])
+        
+        # Afterwards, the annotations have the positions/descriptions of each separate annotation.
+        print(data["responses"][0]["textAnnotations"][1]["description"])
+        print("\n\nNEXT\n\n")
+
+
+bucket = "johnnys-receipt-bucket"
+target_blob = "lmao-blob"
+image = "receipt-ocr-original.jpg"
+# upload_blob("johnnys-receipt-bucket", "target-receipt.jpg", "lmao-blob")
+# file = 'gs://johnnys-receipt-bucket/receipt-ocr-original.jpg'
+resp = annotate_image(bucket, image, False)
+print(resp)
